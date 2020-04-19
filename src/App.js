19a772f96e3061586e0a10febd092f75269ee91d
DESKTOP-1F9V5KA\USER <Aisha>
2020-04-19 23:53:21 +0300

    add music as a const outside components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,63 @@
 import React from "react";
 import "./App.css";
 
+const music = [
+  {
+    id: "Sound1",
+    key: "Q",
+    keyCode: "88",
+    url: "https://www.myinstants.com/media/sounds/snare.mp3",
+  },
+  {
+    id: "Sound2",
+    key: "W",
+    keyCode: "88",
+    url: "https://www.myinstants.com/media/sounds/bass-drum.mp3",
+  },
+  {
+    id: "Sound3",
+    key: "E",
+    keyCode: "88",
+    url: "http://tipiwiki.free.fr/snd/Percussion(4e)2.wav",
+  },
+  {
+    id: "Sound4",
+    key: "A",
+    keyCode: "88",
+    url: "http://www.denhaku.com/r_box/sr16/sr16tom/loelectm.wav",
+  },
+  {
+    id: "Sound5",
+    key: "S",
+    keyCode: "88",
+    url: "http://billor.chsh.chc.edu.tw/sound/bass4.wav",
+  },
+  {
+    id: "Sound6",
+    key: "D",
+    keyCode: "88",
+    url: "http://david.guerrero.free.fr/Effects/ShotgunReload.wav",
+  },
+  {
+    id: "Sound7",
+    key: "Z",
+    keyCode: "88",
+    url: "http://www.denhaku.com/r_box/tr707/closed.wav",
+  },
+  {
+    id: "Sound8",
+    key: "X",
+    keyCode: "88",
+    url: "http://www.masterbits.de/sc_docu/sounds1/1TM00013.MP3",
+  },
+  {
+    id: "Sound9",
+    key: "C",
+    keyCode: "88",
+    url: "http://www.sa-matra.net/sounds/starcontrol/Umgah-Backzip.wav",
+  },
+];
+
 const App = () => (
   <div id="drum-machine">
     <div id="drum-machine">
